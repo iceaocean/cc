52da11a21c719c79f3113178ad1892b9c47ac1ca
root <xiao7.h@qq.com>
2018-12-18 00:23:48 +0800

    cc项目部署

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: main.cpp
+    > Author: qhan
+	> Brief: main.cpp文件
+    > Created Time: Tue 18 Dec 2018 12:22:46 AM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
